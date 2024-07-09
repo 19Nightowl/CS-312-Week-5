837ed3505bd03c2f876bf2283d4a2bfe4929759b
Robin Pace <robin.pace19@gmail.com>
2024-07-09 14:40:18 -0700

    Upload

---
+++ src/compenents/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div class = "note">
+            <h1> This is a Note</h1>
+            <p> This is the content of the note </p>
+        </div>
+    );
+}
+
+
+export default Note;
